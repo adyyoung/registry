6d730b53f1425853d1f36bc396232dcd97d44930
Adrian Young <ady.young@gmail.com>
2020-05-21 00:33:08 +0100

    Update fun.ts

--- fun.ts
+++ fun.ts
@@ -1,2 +1,2 @@
-export const mult(a:number, b: number) => a * b;
-export const add(a:number, b: number) => a + b;
+export const mult = (a:number, b: number) => a * b;
+export const add = (a:number, b: number) => a + b;
